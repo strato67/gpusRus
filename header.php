e9fb2a76f6aefd566c30d5490c2bb9471c47b33d
Saaransh Sharma <saaranshsharma28@gmail.com>
2021-05-22 13:12:26 -0400

    new

--- header.php
+++ header.php
@@ -7,17 +7,17 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <header>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar w/ text</a>
+  <a class="navbar-brand" href="#">GPUS R US</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="shop.php">Shop</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Pricing</a>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include 'header.php';
 
 <html>
 <head>
-
+<link rel="stylesheet" href="styles/indexStyle.css">
 
 
 </head>
@@ -15,7 +15,20 @@ include 'header.php';
 
 <div class="product-slide">
 
-<img src="https://static.techspot.com/images/products/2016/graphics-cards/org/2018-06-08-product-14.jpg">
+<div class="">
+<img src="https://images.anandtech.com/doci/13796/RTX2060_Car_678x452.jpg" alt="RTX 2060">
+
+
+</div>
+<div class="">
+<img src="https://assets1.ignimgs.com/2020/09/24/rtx-3090-blogroll-1600909176729.jpg" alt="RTX 3090">
+</div>
+<div class="">
+<img src="https://www.extremetech.com/wp-content/uploads/2020/09/Radeon-6000-Feature-640x353.jpg" alt="Radeon 6900 XT">
+</div>
+
+
+
 
 
 
