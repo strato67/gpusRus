4d04dc4615f4cf6e5d0840d53f70c293d1409db0
Saaransh Sharma <saaranshsharma28@gmail.com>
2021-08-10 14:07:03 -0400

    nw

--- login.php
+++ login.php
@@ -5,11 +5,39 @@ include 'header.php';
 ?>
 <html>
 
-<h1>
+<body>
 
-Hello
-</h1>
+<div class="loginForm">
+<h1>Login</h1>
+<form action="" method="post">
 
+<div class="container">
+  <div class="email">
+  <input type="email" id="eaddress" name="eaddress" placeholder="Email" required>
+  </div>
+  <div class="password">
+  <input type="password" name="pass" id="pass" placeholder="Password" require>
+  </div>
+  
+  <div class="submit">
+    <input type="submit" value="Submit">
+
+  </div>
+</div>
+
+
+
+
+</form>
+
+
+
+</div>
+
+
+
+
+</body>
 
 
 
--- signup.php
+++ signup.php
@@ -6,31 +6,56 @@ include 'header.php';
 
 <html>
 <head>
-<link rel="stylesheet" href="styles/signupStyle.css">
+<link rel="stylesheet" href="styles/signStyle.css">
 
 </head>
 
+<body>
 
 <div class="signupForm">
 <h1>Sign Up</h1>
 <form action="" method="post">
 
+<div class="container">
+  <div class="email">
+  <input type="email" id="eaddress" name="eaddress" placeholder="Email" required>
+  </div>
+  <div class="password">
+  <input type="password" name="pass" id="pass" placeholder="Password" require>
+  </div>
+  <div class="first-name">
+  <input type="text" id="fname" name="fname" placeholder="First Name" required> 
+  </div>
+  <div class="last-name">
+  <input type="text" id="lname" name="lname" placeholder="Last Name" required>
+  </div>
+  <div class="address">
+  <input type="text" id="address" name="address" placeholder="Address" required>
+  </div>
+  <div class="phone-number">
+  <input type="tel" id="phone" name="phone" placeholder="Phone Number" pattern="[0-9]{10}" required>
+  </div>
+  <div class="submit">
+    <input type="submit" value="Submit">
+
+  </div>
+</div>
 
 
 
-<input type="email" id="eaddress" name="eaddress" placeholder="Email" required>
-<input type="password" name="pass" id="pass" placeholder="Password" require>
-<input type="text" id="fname" name="fname" placeholder="First Name" required> 
-<input type="text" id="lname" name="lname" placeholder="Last Name" required>
-<input type="tel" id="phone" name="phone" placeholder="Phone Number" pattern="[0-9]{10}" required>
-
-<input type="submit" value="Submit">
 
 </form>
 
 
 
 </div>
+
+
+
+
+</body>
+
+
 
 
 </html>
